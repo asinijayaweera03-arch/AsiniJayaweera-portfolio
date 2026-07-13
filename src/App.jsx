f551f55827e23181d2b7e32016c258ee1b01e86d
asinijayaweera <asinijayaweera03@gmail.com>
2026-07-13 08:30:24 +0530

    feat: compose page layout in App component

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Certificates from './components/Certificates';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <Navbar />
+      <main className="main-content">
+        <Hero />
+        <About />
+        <Projects />
+        <Skills />
+        <Certificates />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
